da6b701041712b3b831687f142cc6def28241147
William Machado Costa <willian.costa@cappta.com.br>
2017-07-21 12:04:56 -0300

    alteração na forma de consultar incluindo o id

--- CapptaDev1/Dados/CarroPopular.cs
+++ CapptaDev1/Dados/CarroPopular.cs
@@ -8,12 +8,16 @@ namespace CapptaDev1
 {
     public class CarroPopular:Carro
     {
-
+        //abri conexão
         ConectaBanco conectaBanco = new ConectaBanco();
+
+        //ABSTRACT
+
+
         //metodo para adicionar novo carro
         public  override void  adicionarCarro(Carro carro)
         {
-            string sql = "insert into carroPopular (carro, modelo, ano,preco) values(@carro,@modelo,@ano,@preco)";
+            string sql = "insert into carroPopular (carro, modelo, ano,preco, marca, quantidade) values(@carro,@modelo,@ano,@preco,@marca, @quantidade)";
             try
             {
                 conectaBanco.abrirConexao();
@@ -22,6 +26,8 @@ namespace CapptaDev1
                 conectaBanco.comand.Parameters.AddWithValue("@modelo", carro.modelo);
                 conectaBanco.comand.Parameters.AddWithValue("@ano", carro.ano);
                 conectaBanco.comand.Parameters.AddWithValue("@preco", carro.preco);
+                conectaBanco.comand.Parameters.AddWithValue("@marca", carro.marca);
+                conectaBanco.comand.Parameters.AddWithValue("@quantidade", carro.quantidade);
 
                 conectaBanco.comand.ExecuteNonQuery();
             }
@@ -38,18 +44,20 @@ namespace CapptaDev1
         public override void alterarCarro(Carro carro)
         {
            
-           
+         
                 try
                 {
                     conectaBanco.abrirConexao();
-                    conectaBanco.comand = new MySql.Data.MySqlClient.MySqlCommand("update carroPopular carro=@carro, modelo=@modelo, ano=@ano, preco =@preco where id = @id", conectaBanco.sqlConection);
+                    conectaBanco.comand = new MySql.Data.MySqlClient.MySqlCommand("update carroPopular set carro=@carro, modelo=@modelo, ano=@ano, preco =@preco, marca= @marca, quantidade=@quantidade where id = @id", conectaBanco.sqlConection);
 
                     conectaBanco.comand.Parameters.AddWithValue("@carro", carro.carro);
                     conectaBanco.comand.Parameters.AddWithValue("@modelo", carro.modelo);
                     conectaBanco.comand.Parameters.AddWithValue("@ano", carro.ano);
                     conectaBanco.comand.Parameters.AddWithValue("@preco", carro.preco);
+                    conectaBanco.comand.Parameters.AddWithValue("@marca", carro.marca);
+                    conectaBanco.comand.Parameters.AddWithValue("@quantidade", carro.quantidade);
 
-                    conectaBanco.comand.Parameters.AddWithValue("@id", carro.id);
+                conectaBanco.comand.Parameters.AddWithValue("@id", carro.id);
 
                  conectaBanco.comand.ExecuteNonQuery();
 
@@ -65,26 +73,117 @@ namespace CapptaDev1
             
         }
 
+        //metodo para listar carro
+        public override List<Carro> listarCarro()
+        {
+            try
+            {
+                ConectaBanco conectaBanco = new ConectaBanco();
+                conectaBanco.abrirConexao();
+                String sql = "select*from carropopular";
+
+                conectaBanco.comand = new MySql.Data.MySqlClient.MySqlCommand(sql, conectaBanco.sqlConection);
+                conectaBanco.dataReader = conectaBanco.comand.ExecuteReader();
+
+                List<Carro> lista = new List<Carro>();
+
+                while (conectaBanco.dataReader.Read())
+                {
+                    CarroPopular carro = new CarroPopular();
+                    carro.id = Convert.ToInt32(conectaBanco.dataReader["id"]);
+                    carro.carro = Convert.ToString(conectaBanco.dataReader["carro"]);
+                    carro.modelo = Convert.ToString(conectaBanco.dataReader["modelo"]);
+                    carro.ano = Convert.ToString(conectaBanco.dataReader["ano"]);
+                    carro.preco = Convert.ToString(conectaBanco.dataReader["preco"]);
+                    carro.marca = Convert.ToString(conectaBanco.dataReader["marca"]);
+                    carro.quantidade = Convert.ToString(conectaBanco.dataReader["quantidade"]);
+
+
+                    lista.Add(carro);
+                    
+
+                }
+
+                return lista;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
+        // metodo para listar carro por nome
+        public override List<Carro> ListarCarroPorNome(string nome)
+        {
+            try
+            {
+                ConectaBanco conectaBanco = new ConectaBanco();
+                conectaBanco.abrirConexao();
+                String sql = "select id, carro, modelo, ano, preco, marca,quantidade from carropopular where (carro LIKE '%' @carro '%')";
+
+                conectaBanco.comand = new MySql.Data.MySqlClient.MySqlCommand(sql, conectaBanco.sqlConection);
+                conectaBanco.comand.Parameters.AddWithValue("@carro", nome);
+                conectaBanco.dataReader = conectaBanco.comand.ExecuteReader();
+
+                List<Carro> lista = new List<Carro>();
+
+                //Criando um local na memória
+                // carroPopular = null;
+                while (conectaBanco.dataReader.Read())
+                {
+                    CarroPopular carroPopular = new CarroPopular();
+
+                    carroPopular.id = Convert.ToInt32(conectaBanco.dataReader["id"]);
+                    carroPopular.carro = Convert.ToString(conectaBanco.dataReader["carro"]);
+                    carroPopular.modelo = Convert.ToString(conectaBanco.dataReader["modelo"]);
+                    carroPopular.ano = Convert.ToString(conectaBanco.dataReader["ano"]);
+                    carroPopular.preco = Convert.ToString(conectaBanco.dataReader["preco"]);
+                    carroPopular.marca = Convert.ToString(conectaBanco.dataReader["marca"]);
+                    carroPopular.quantidade = Convert.ToString(conectaBanco.dataReader["quantidade"]);
+
+                    lista.Add(carroPopular);
+                }
+
+                return lista;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
         //metodo para pesquisar por nome
-        public override Carro pesquisarPorNome(string nome)
+        public override CarroPopular pesquisarPorNome(string nome)
         {
             try
             {
                 conectaBanco.abrirConexao();
-                String sql = "Select*from carroPopular where nome=@nome";
+                String sql = "select id, carro, modelo, ano, preco, marca, quantidade from carropopular where carro = @carro";
                 conectaBanco.comand = new MySql.Data.MySqlClient.MySqlCommand(sql, conectaBanco.sqlConection);
 
-                conectaBanco.comand.Parameters.AddWithValue("@id", nome);
+                conectaBanco.comand.Parameters.AddWithValue("@carro", nome);
+
+                conectaBanco.dataReader = conectaBanco.comand.ExecuteReader();
+
                 //Criando um local na memória
                 CarroPopular carroPopular = null;
                 if (conectaBanco.dataReader.Read())
                 {
                     carroPopular = new CarroPopular();
 
-                    carroPopular.carro = Convert.ToString(conectaBanco.dataReader["nome"]);
+                    carroPopular.id = Convert.ToInt32(conectaBanco.dataReader["id"]);
+                    carroPopular.carro = Convert.ToString(conectaBanco.dataReader["carro"]);
                     carroPopular.modelo = Convert.ToString(conectaBanco.dataReader["modelo"]);
                     carroPopular.ano = Convert.ToString(conectaBanco.dataReader["ano"]);
                     carroPopular.preco = Convert.ToString(conectaBanco.dataReader["preco"]);
+                    carroPopular.marca = Convert.ToString(conectaBanco.dataReader["marca"]);
+                    carroPopular.quantidade = Convert.ToString(conectaBanco.dataReader["quantidade"]);
 
                 }
 
@@ -96,5 +195,91 @@ namespace CapptaDev1
                 throw new Exception(mensagem + ".. não foi possivel encontrar o produto informado");
             }
         }
+
+
+        //metodo para pesquisar por nome e retornar a quantidade
+        public override CarroPopular pesquisarPorNomeQuantidade(string nome)
+        {
+            try
+            {
+                conectaBanco.abrirConexao();
+                String sql = "select  quantidade from carropopular where carro = @carro";
+                conectaBanco.comand = new MySql.Data.MySqlClient.MySqlCommand(sql, conectaBanco.sqlConection);
+
+                conectaBanco.comand.Parameters.AddWithValue("@carro", nome);
+
+                conectaBanco.dataReader = conectaBanco.comand.ExecuteReader();
+
+                //Criando um local na memória
+                CarroPopular carroPopular = null;
+                if (conectaBanco.dataReader.Read())
+                {
+                    carroPopular = new CarroPopular();
+
+
+                    carroPopular.quantidade = Convert.ToString(conectaBanco.dataReader["quantidade"]);
+
+                }
+
+                return carroPopular;
+            }
+            catch (Exception mensagem)
+            {
+
+                throw new Exception(mensagem + ".. não foi possivel encontrar o produto informado");
+            }
+        }
+
+        //------------------------------------------------------------------
+        // NÃO ABSTRATO
+
+
+
+
+
+        //metodo para pesquisar por código
+        public  CarroPopular pesquisarCarroPorCodigo(int codigo)
+        {
+            try
+            {
+                conectaBanco.abrirConexao();
+                String sql = "select id, carro, modelo, ano, preco, marca,quantidade from carropopular where id = @id";
+                conectaBanco.comand = new MySql.Data.MySqlClient.MySqlCommand(sql, conectaBanco.sqlConection);
+
+                conectaBanco.comand.Parameters.AddWithValue("@id", codigo);
+
+                conectaBanco.dataReader = conectaBanco.comand.ExecuteReader();
+
+                //Criando um local na memória
+                CarroPopular carroPopular = null;
+                if (conectaBanco.dataReader.Read())
+                {
+                    carroPopular = new CarroPopular();
+
+                    carroPopular.id = Convert.ToInt32(conectaBanco.dataReader["id"]);
+                    carroPopular.carro = Convert.ToString(conectaBanco.dataReader["carro"]);
+                    carroPopular.modelo = Convert.ToString(conectaBanco.dataReader["modelo"]);
+                    carroPopular.ano = Convert.ToString(conectaBanco.dataReader["ano"]);
+                    carroPopular.preco = Convert.ToString(conectaBanco.dataReader["preco"]);
+                    carroPopular.marca = Convert.ToString(conectaBanco.dataReader["marca"]);
+                    carroPopular.quantidade = Convert.ToString(conectaBanco.dataReader["quantidade"]);
+
+                }
+
+                return carroPopular;
+            }
+            catch (Exception mensagem)
+            {
+
+                throw new Exception(mensagem + ".. não foi possivel encontrar o produto informado");
+            }
+        }
+
+
+
+
+        
+
+       
     }
 }
