2a160dd03854eb6ccadf9c42be23c45b70df6cce
William Machado Costa <willian.costa@cappta.com.br>
2017-07-21 12:21:54 -0300

    Criação de classe Compra posteiormente abandonado

---
+++ CapptaDev1/Bean/Compra.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapptaDev1.Bean
+{
+    public  class Compra
+    {
+
+        public void ComprarCarro()
+        {
+
+        }
+    }
+}
