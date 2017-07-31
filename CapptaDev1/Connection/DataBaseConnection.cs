d5be57f7f5ca111f4637ad27794faa4039b66bad
William Machado Costa <willian.costa@cappta.com.br>
2017-07-31 14:34:23 -0300

    Criação de classe de conexão

---
+++ CapptaDev1/Connection/DataBaseConnection.cs
@@ -0,0 +1,16 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace CapptaDev1
+{
+    public static class DataBaseConnection
+    {
+        public static MySqlConnection open()
+        {
+            var sql = "server=localhost;user id=root;password=master;database=carros";
+            var sqlConnection = new MySqlConnection(sql);
+            sqlConnection.Open();
+
+            return sqlConnection;
+        }
+    }
+}
