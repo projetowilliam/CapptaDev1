f4c563206bad9dc18a10855ea47a942bc35fd181
William Machado Costa <willian.costa@cappta.com.br>
2017-07-31 14:43:59 -0300

    testando conexão

---
+++ CapptaDev1/Resources/Mensagem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapptaDev1
+{
+    public  class Mensagem: Exception
+    {
+
+       
+    }
+}
