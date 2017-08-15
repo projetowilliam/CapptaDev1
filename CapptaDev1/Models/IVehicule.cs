644b0049229b6eae56d23cb1fbb94685c4088500
William Machado Costa <willian.costa@cappta.com.br>
2017-08-15 19:17:00 -0300

    alteração dos pacotes

---
+++ CapptaDev1/Models/IVehicule.cs
@@ -0,0 +1,12 @@
+﻿namespace CapptaDev1.Models
+{
+    public interface IVehicule
+    {
+        int id { get; set; }
+        string name { get; set; }
+        string model { get; set; }
+        string year { get; set; }
+        string price { get; set; }
+        string mark { get; set; }
+    }
+}
